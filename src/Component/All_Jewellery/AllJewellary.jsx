d5cafcbfb46a8abbe3b782a09f648c429db1a94d
lucky <pr0216805@gmail.com>
2025-03-13 19:01:48 +0530

    Fixed all pages routing

--- src/Component/All_Jewellery/AllJewellary.jsx
+++ src/Component/All_Jewellery/AllJewellary.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ProductCard from '../ProductsCard/ProductCard'
+import jewelleryimg1 from '../../assets/jewelleryimg1.jpg'
+import jewelleryimg2 from '../../assets/jewelleryimg2.jpg'
+import jewelleryimg3 from '../../assets/jewelleryimg3.jpg'
+import jewelleryimg4 from '../../assets/jewelleryimg4.jpg'
+import jewelleryimg5 from '../../assets/jewelleryimg5.jpg'
+import FilterSidebar from '../Filter/FilterSidebar'
+import FilterButton from '../Filter/FilterButton'
+
+
+const jewelleryData = [
+  { img: jewelleryimg1, title: "Dazzling Grace Drop Earrings", price: "₹ 50784" },
+  { img: jewelleryimg2, title: "Dazzling Grace Drop Earrings", price: "₹ 40564" },
+  { img: jewelleryimg3, title: "Dazzling Grace Drop Earrings", price: "₹ 45677" },
+  { img: jewelleryimg4, title: "Dazzling Grace Drop Earrings", price: "₹ 12345" },
+  { img: jewelleryimg5, title: "Dazzling Grace Drop Earrings", price: "₹ 98765" },
+  { img: jewelleryimg2, title: "Dazzling Grace Drop Earrings", price: "₹ 67548" }
+]
 
 function AllJewellary() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(true)
+  }
+  const clickHandle=()=>{
+    setIsOpen(false);
+  }
+
+
   return (
-    <div>
-      this is all AllJewellary page
-    </div>
+    <>
+      <h1 className='text-black lg:text-2xl font-bold p-2 lg:ml-8 my-3 md:text-left text-center'>
+        All Jewelleryes (12345)
+      </h1>
+      <FilterButton handleClick={handleClick} />
+      {isOpen && <FilterSidebar clickHandle={clickHandle}/>}
+      <div className=' grid md:grid-cols-2 lg:grid-cols-3'>
+        {jewelleryData.map((info, index) => (
+          <ProductCard
+            key={index}
+            titleimg={info.img}
+            title={info.title}
+            price={info.price}
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
--- src/Component/Diamond/Diamond_Rings.jsx
+++ src/Component/Diamond/Diamond_Rings.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import ProductCard from '../ProductsCard/ProductCard'
+import diamondimg1 from '../../assets/diamondimg1.jpg'
+import diamondimg2 from '../../assets/diamondimg2.jpg'
+import diamondimg3 from '../../assets/diamondimg3.jpg'
+import diamondimg4 from '../../assets/diamondimg4.jpg'
+import diamondimg5 from '../../assets/diamondimg5.jpg'
+import diamondimg6 from '../../assets/diamondimg6.jpg'
+
+
+const diamondData = [
+  { img: diamondimg1, title: "Dazzling Grace Drop Earrings", price: "₹ 50784" },
+  { img: diamondimg2, title: "Dazzling Grace Drop Earrings", price: "₹ 40564" },
+  { img: diamondimg3, title: "Dazzling Grace Drop Earrings", price: "₹ 45677" },
+  { img: diamondimg4, title: "Dazzling Grace Drop Earrings", price: "₹ 12345" },
+  { img: diamondimg5, title: "Dazzling Grace Drop Earrings", price: "₹ 98765" },
+  { img: diamondimg6, title: "Dazzling Grace Drop Earrings", price: "₹ 67548" }
+]
 
 function Diamond_Rings() {
   return (
-    <div>
-      diamond rings page
-    </div>
+    <>
+      <h1 className='text-black lg:text-2xl font-bold p-2 lg:ml-8 my-3 md:text-left text-center'>
+        Diamonds Rings (65748)
+      </h1>
+      <div className=' grid md:grid-cols-2 lg:grid-cols-3'>
+        {diamondData.map((info, index) => (
+          <ProductCard
+            key={index}
+            titleimg={info.img}
+            title={info.title}
+            price={info.price}
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
